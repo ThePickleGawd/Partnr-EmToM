72a34c0d30ad3823c68973355bc4df28b1544470
Dylan Lu <dylanelu@gmail.com>
2025-12-01 10:40:19 -0800

    fixing prompts

--- bomb_game_run.sh
+++ bomb_game_run.sh
@@ -10,7 +10,7 @@ set +a
 python -m habitat_llm.examples.emtom --config-name game/bomb_game \
     mode="cli" \
     evaluation.save_video=True \
-    +game.manual_agents=[0,1] \
+    +game.manual_agents=[] \
     +evaluation.save_fpv_stills=False \
     # +game.manual_obs_popup=true \ # I think this is for manual popup
     # habitat.environment.max_episode_steps=10000 \
